12ad1b7fe927c27232619d9773776da89d670a0c
Jared Rose <lfj143@gmail.com>
2021-07-01 22:55:28 -0700

    barebones website

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+// bootstrap a table
+// fill bootstrap table with IDs and content
+// link js with IDs= consoleLog EVERYTHING
+// moment live time with past/present/future css code
+
+// get the element that the info goes into
+// decide on what it should look like
+// and then update it...
+// should this run in a loop and update the clock reguraly?
+// create timeblocks with inputs and buttons
+// loop and dynamically or I could hard code it.
+// add events to the submit button to save what is in the
+//     input to local storage
+//  get the value out of local storage and populate
+//     the input with the data
+// check to see what the color of the input should be based on the time
+//      moment?
+// look at the css for classes later for this
+// store data in localstrage as an object where the key is the hour of the day...
